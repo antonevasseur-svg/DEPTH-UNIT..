dd4926eb9f936d39080b1b635fedb664573a1eda
antonevasseur-svg <antonevasseur@gmail.com>
2026-08-16 19:16:01 +0200

    Connect cursor and audio providers to the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import { CursorProvider } from './store/cursorStore';
+import { AudioProvider } from './store/audioStore';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <CursorProvider>
+          <AudioProvider>
+            <App />
+          </AudioProvider>
+        </CursorProvider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
